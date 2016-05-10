852cfff2314c0075c5ed948b6686190b7d0c4dc1
egorskor <egorskor1995@mail.ru>
2016-05-10 19:24:28 +0300

    JQuery

--- javascript.js
+++ javascript.js
@@ -1,16 +1,25 @@
+
 function calc() {
     var a = +document.getElementById('input1').value;
     var b = +document.getElementById('input2').value;
-    var c = +document.getElementById('input3').value;
+    /*var c = +document.getElementById('input3').value;*/
+    var c =+$("#input3").val();
     var p = (a + b + c) / 2;
     if (check(a, b, c)) {
-        document.getElementById('input4').value = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        var answer=Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        /*document.getElementById('input4').value = answer;*/
+        $("#table").append("<tr><td>"+a+"</td><td>"+b+"</td><td>"+c+"</td><td>"+answer+"</td></tr>");
+        $("#window").hide();
+        $(".content").show();
+        $("body").css("background-color", "lightblue");
     }
     else {
-        alert("Incorrect input");
+        /*alert("Incorrect input");*/
+        $(".mistake").text("Invalid input");
+        $(".mistake").show();
     }
-}
 
+}
 function check(a, b, c) {
     if (isNumeric(a) && isNumeric(b) && isNumeric(c)) {
         if ((a < b + c) && (b < a + c) && (c < a + b)) {
